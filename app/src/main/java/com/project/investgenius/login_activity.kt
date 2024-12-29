d20a7f96b38da2a9bea7268858221ec94fb19ef6
InvestGenius <karbeleprathamesh@gmail.com>
2024-12-29 19:29:16 +0530

    lot of changes

--- app/src/main/java/com/project/investgenius/login_activity.kt
+++ app/src/main/java/com/project/investgenius/login_activity.kt
@@ -28,7 +28,7 @@ class login_activity : AppCompatActivity() {
 
         binding.button2.setOnClickListener{
             val email = binding.editTextTextEmailAddress.text.toString()
-            val password = binding.editTextTextPassword.text.toString()
+            val password = binding.passtext.text.toString()
             if (email.isEmpty()||password.isEmpty()){
                 Toast.makeText(this, "Please Fill Credentials", Toast.LENGTH_SHORT).show()
             }
